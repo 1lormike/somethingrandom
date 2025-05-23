5dd11d4188f7df9535fc79139c14f4b87c1e2781
1lormike <mowolabi225@gmail.com>
2025-05-23 10:05:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Marcus.vy == 0) {
+        Marcus.vy = -150
+    }
+})
+let Marcus: Sprite = null
+scene.setBackgroundColor(9)
+Marcus = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Marcus, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+Marcus.ay = 350
+scene.cameraFollowSprite(Marcus)
