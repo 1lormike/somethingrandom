d30e79ad407db064309b39860f5e0def068c6885
MrDum1 <149846178+MrDum1@users.noreply.github.com>
2025-05-26 23:07:39 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "j/f@~86qvv]UT)Y+7)Jf":
+            case "Player":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f c c c c c c f . . . . 
+. . . . f c c c c c c f . . . . 
+. . . . f c c 1 c 1 c f . . . . 
+. . . . f c c 1 c 1 c f . . . . 
+. . . . f c c c c c c f . . . . 
+. . . . f c c c c c c f . . . . 
+. . . f 8 c c c c c f . . . . . 
+. . f 8 8 c c c c c 2 f . . . . 
+. . f 8 8 c c c c c 2 2 f . . . 
+. . . f f 1 1 c c c 2 2 f . . . 
+. . 1 1 1 1 9 c c 3 3 f . . . . 
+. . . f 9 9 9 f f 3 3 f . . . . 
+. . . f 9 9 f . f 3 3 f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -5,24 +5,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let Marcus: Sprite = null
 scene.setBackgroundColor(9)
-Marcus = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . f c c c c c c f . . . . 
-    . . . . f c c c c c c f . . . . 
-    . . . . f c c 1 c 1 c f . . . . 
-    . . . . f c c 1 c 1 c f . . . . 
-    . . . . f c c c c c c f . . . . 
-    . . . . f c c c c c c f . . . . 
-    . . . f 8 c c c c c f . . . . . 
-    . . f 8 8 c c c c c 2 f . . . . 
-    . . f 8 8 c c c c c 2 2 f . . . 
-    . . . f f 1 1 c c c 2 2 f . . . 
-    . . 1 1 1 1 9 c c 3 3 f . . . . 
-    . . . f 9 9 9 f f 3 3 f . . . . 
-    . . . f 9 9 f . f 3 3 f . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+Marcus = sprites.create(assets.image`Player`, SpriteKind.Player)
 controller.moveSprite(Marcus, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
 Marcus.ay = 350
